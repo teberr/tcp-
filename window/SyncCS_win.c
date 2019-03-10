b208ba21fe49df8b4ad738a46e3902f131694bf8
teberr <43701168+teberr@users.noreply.github.com>
2019-03-10 20:44:29 +0900

    SyncCS_win

---
+++ window/SyncCS_win.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <windows.h>
+#include <process.h>
+
+
+#define NUM_THREAD 50
+long long num = 0;
+CRITICAL_SECTION cs;
+
+unsigned WINAPI threadInc(void * arg) {
+	int i;
+	EnterCriticalSection(&cs);
+	for (i = 0; i < 50000000; i++)
+		num += 1;
+	LeaveCriticalSection(&cs);
+	return 0;
+}
+
+unsigned WINAPI threadDes(void *arg) {
+	int i;
+	EnterCriticalSection(&cs);
+	for (i = 0; i < 50000000; i++)
+		num -= 1;
+	LeaveCriticalSection(&cs);
+	return 0;
+}
+
+
+int main(int argc, char *argv[]) {
+
+	HANDLE tHandles[NUM_THREAD];
+	int i;
+
+	InitializeCriticalSection(&cs);
+	for (i = 0; i < NUM_THREAD; i++) {
+		if (i % 2)
+			tHandles[i] = (HANDLE)_beginthreadex(NULL, 0, threadInc, NULL, 0, NULL);
+		else
+			tHandles[i] = (HANDLE)_beginthreadex(NULL, 0, threadDes, NULL, 0, NULL);
+	}
+	WaitForMultipleObjects(NUM_THREAD, tHandles, TRUE, INFINITE);
+	DeleteCriticalSection(&cs);
+	printf("result: %lld \n", num);
+	return 0;
+}
