b7f7e42385fe21c8b77512d0a41122718be5192a
teberr <43701168+teberr@users.noreply.github.com>
2019-03-12 00:30:48 +0900

    SyncSema

---
+++ window/SyncSema_win.c
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+#include <windows.h>
+#include <process.h>
+
+
+static HANDLE semOne;
+static HANDLE semTwo;
+static int num;
+
+unsigned WINAPI Read(void * arg) {
+	int i;
+	for (i = 0; i < 5; i++) {
+		fputs("input num:", stdout);
+		WaitForSingleObject(semTwo, INFINITE);
+		scanf("%d", &num);
+		ReleaseSemaphore(semOne, 1, NULL);
+
+	}
+	return 0;
+}
+unsigned WINAPI Accu(void * arg) {
+	int sum = 0, i;
+	for (i = 0; i < 5; i++) {
+		WaitForSingleObject(semOne, INFINITE);
+		sum += num;
+		ReleaseSemaphore(semTwo, 1, NULL);
+
+	}
+	printf("Result: %d \n", sum);
+	return 0;
+}
+int main(int argc, char * argv[]) {
+	HANDLE hThread1, hThread2;
+	semOne = CreateSemaphore(NULL, 0, 1, NULL);
+	semTwo = CreateSemaphore(NULL, 1, 1, NULL);
+
+	hThread1 = (HANDLE)_beginthreadex(NULL, 0, Read, NULL, 0, NULL);
+	hThread2 = (HANDLE)_beginthreadex(NULL, 0, Accu, NULL, 0, NULL);
+
+	WaitForSingleObject(hThread1, INFINITE);
+	WaitForSingleObject(hThread2, INFINITE);
+
+	CloseHandle(semOne);
+	CloseHandle(semTwo);
+	return 0;
+
+}
